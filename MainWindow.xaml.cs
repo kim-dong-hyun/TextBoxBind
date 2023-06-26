184e9d55f89283a9e9e390d61c01eee459eea399
kim dong hyun <dhkim.ants@gmail.com>
2023-06-26 18:08:47 +0900

    저장소 생성

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBoxBind
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string inputText;
+
+        public string InputText
+        {
+            get { return inputText; }
+            set
+            {
+                if (inputText != value)
+                {
+                    Console.WriteLine($"InputText : {value}");
+                    inputText = value;
+                    OnPropertyChanged("InputText");
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            InputText += "AAA";
+        }
+    }
+}
